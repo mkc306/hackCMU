98a7de6c05ad24eb14659f8ba45dadad58f1a915
mkc306 <m3k0c6@gmail.com>
2017-09-08 19:41:38 -0400

    initial commit

---
+++ hackCMU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hackCMU
+//
+//  Created by Michael Kelvyn Chan on 08/09/2017.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
